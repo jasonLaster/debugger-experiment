e18981ad19fc704f99380d113ae9b9d342719b45
bomsy <b4bomsy@gmail.com>
2017-03-07 17:07:44 +0000

    move location function to utils/index

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -40,6 +40,7 @@ const {
   breakpointAtLine,
   getTextForLine,
   getCursorLine,
+  getTokenLocation,
   resizeBreakpointGutter,
   traverseResults
 } = require("../../utils/editor");
@@ -274,10 +275,7 @@ const Editor = React.createClass({
       selectedToken.classList.remove("selected-token");
     }
 
-    const lineOffset = 1;
-    const { left, top } = token.getBoundingClientRect();
-    const { line, ch } = ctx.cm.coordsChar({ left, top });
-    const loc = { line: line + lineOffset, column: ch };
+    const loc = getTokenLocation(token, ctx.cm);
 
     const variables = selectedFrame.scope.bindings.variables;
 
