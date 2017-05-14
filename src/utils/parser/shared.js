cf7534b851a4181e0e6812db1726e2aab3d11f78
Jason Laster <jason.laster.11@gmail.com>
2017-05-15 00:12:56 +0100

    split up the parser

---
+++ src/utils/parser/shared.js
@@ -0,0 +1,27 @@
+const t = require("babel-types");
+
+type Scope = {
+  location: {
+    line: number,
+    column: number
+  },
+  parent: Scope,
+  bindings: Object[]
+};
+
+export function isFunction(path) {
+  return (
+    t.isFunction(path) ||
+    t.isArrowFunctionExpression(path) ||
+    t.isObjectMethod(path) ||
+    t.isClassMethod(path)
+  );
+}
+
+export function isVariable(path) {
+  return (
+    t.isVariableDeclaration(path) ||
+    (isFunction(path) && path.node.params.length) ||
+    (t.isObjectProperty(path) && !isFunction(path.node.value))
+  );
+}
